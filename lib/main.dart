06d84e74b1f40fc9dcea692eda12e38b13173e9e
tameralshazly <tameralshazly@yahoo.com>
2023-09-02 00:34:41 +0400

    step-01

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(child: App()),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      home: const HomePage(),
+    );
+  }
+}
+
+const names = [
+  'Alice',
+  'Bob',
+  'Charlie',
+  'David',
+  'Eve',
+  'Fred',
+  'Ginny',
+  'Harriet',
+  'Ileans',
+  'Joseph',
+  'Kincaid',
+  'Larry'
+];
+
+final tickerProvider = StreamProvider(
+    (ref) => Stream.periodic(const Duration(seconds: 1), (i) => i + 1));
+
+final namesProvider = StreamProvider((ref) async* {
+  yield* ref
+      .watch(tickerProvider.stream)
+      .map((count) => names.getRange(0, count));
+});
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final names = ref.watch(namesProvider);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Stream provider"),
+      ),
+      body: names.when(
+        data: (names) {
+          return ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(names.elementAt(index)),
+              );
+            },
+          );
+        },
+        error: (error, stackTrace) => const Text('Reached the end of the list'),
+        loading: () => const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
